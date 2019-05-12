22ef90cb33b59758c91b67d00398b413e1264cb6
yuvalmor <yuvalmor94@gmail.com>
2019-05-12 16:13:36 +0300

    finished

--- ex32.c
+++ ex32.c
@@ -170,67 +170,93 @@ void searchFileInDirectory(char* directoryPath, char* filePath){
 }
 
 /**
- *
+ * The function RunStudentFile - runs the C file found in the student directory.
+ * It checks if the program running time take less then five sec.
+ * It returns true (1) if the program finished until five sec,
+ * And false otherwise.
+ * @param inputPath - the input to the program
+ * @param studentName - the student that the file belong to
+ * @return - true (1) if the program running time took under five sec,
+ * And false (0) otherwise.
  */
 int RunStudentFile(char* const inputPath, char const* studentName){
     pid_t pid;
     int status;
     int time=INITIAL_VALUE;
-
     char outputFileName[MAX_CHAR_IN_LINE]={};
+    //Set the name of the output text according to the student name
     strcpy(outputFileName,studentName);
     strcat(outputFileName,END_OF_TXT);
-
     char* const args[] ={EXECUTE_COMPILED_FILE,NULL};
+    // Create child process
     if((pid = fork()) == SYSTEM_CALL_FAILURE){
         EndProgram();
     }
+    // If its the child process
     if(pid == CHILD_PID) {
         int in;
         int out;
+        // Open the file that contain the input to the program
         in = open(inputPath,O_RDONLY);
+        // Check system call open
         if (in == SYSTEM_CALL_FAILURE) {
             EndProgram();
         }
+        // open (and create if not exist) the output program file
         out = open(outputFileName,O_WRONLY |O_TRUNC|O_CREAT,PERMISSION);
+        //Check system call open
         if (out == SYSTEM_CALL_FAILURE) {
             close(in);
             EndProgram();
         }
-        // Replace standard input with input file
+        // Replace standard input with input file and check validation
         if (dup2(in, STANDARD_INPUT) == SYSTEM_CALL_FAILURE) {
             close(in);
             close(out);
             EndProgram();
         }
+        // Replace standard output with output file and check validation
         if (dup2(out, STANDARD_OUTPUT) == SYSTEM_CALL_FAILURE) {
             close(in);
             close(out);
             EndProgram();
         }
-        close(in);
-        close(out);
+        // Close both of the files and check
+        if(close(in)==SYSTEM_CALL_FAILURE || close(out)==SYSTEM_CALL_FAILURE){
+            EndProgram();
+        }
+        // Run the program
         execvp(args[INITIAL_VALUE], args);
         EndProgram();
     }
+    // Father process
+    // Checks if the kid proccess ended until 5 seconds
     while (time<TIMEOUT && !(waitpid(pid,&status,WNOHANG))){
         time++;
         sleep(SEC);
     }
+    // The kid process didn't finish in time
     if(time==TIMEOUT){
         return INITIAL_VALUE;
     }
+    // The kid process finish in time
     return SUCCESS;
 }
 
-
-signed int CompileStudentFile(char* const studentFilePath){
+/**
+ * The function CompileStudentFile - compile the student file,
+ * And return true (1) if it succeed, and false (0) otherwise.
+ * @param studentFilePath - the path to the C file
+ * @return- true (1) if the file compiled, and false (0) otherwise.
+ */
+int CompileStudentFile(char* const studentFilePath){
     int status;
     pid_t pid;
     char* const args[] = {GCC_COMMAND,GCC_FLAG,COMPILED_FILE_NAME,studentFilePath,NULL};
     if((pid = fork()) == SYSTEM_CALL_FAILURE){
         EndProgram();
     }
+    // Checks if its the child procces, and compile the file
     if(pid == CHILD_PID){
        execvp(args[INITIAL_VALUE],args);
        EndProgram();
@@ -239,6 +265,10 @@ signed int CompileStudentFile(char* const studentFilePath){
     if(waitpid(pid,&status,INITIAL_VALUE)==SYSTEM_CALL_FAILURE){
         EndProgram();
     }
+    /*
+     * Checks the exit status of the child process,
+     * If he succeed to compile the file or not
+     * */
     if(WEXITSTATUS(status)!=INITIAL_VALUE){
         return INITIAL_VALUE;
     }
@@ -246,13 +276,22 @@ signed int CompileStudentFile(char* const studentFilePath){
 }
 
 
+/**
+ * The function CompareOutput - gets two paths to files,
+ * Compare between the file using the program we write in ex31.
+ * @param outputPath - the correct output file path
+ * @param programOutputPath - the program output file path
+ * @return 1 for identical, 2 for different and 3 for similar
+ */
 int CompareOutput(char* const outputPath, char* const programOutputPath){
     int status;
     pid_t pid;
     char* const args[] = {EXECUTE_COMPILED_COMPARE_PROGRAM,outputPath,programOutputPath,NULL};
+    // Create child process and check
     if((pid = fork()) == SYSTEM_CALL_FAILURE){
         EndProgram();
     }
+    // Checks if ita the child process and run the compare program
     if(pid == CHILD_PID){
         execvp(args[INITIAL_VALUE],args);
         EndProgram();
@@ -261,20 +300,35 @@ int CompareOutput(char* const outputPath, char* const programOutputPath){
     if(waitpid(pid,&status,INITIAL_VALUE)==SYSTEM_CALL_FAILURE){
         EndProgram();
     }
+    // Return the value that returned from the program
     return WEXITSTATUS(status);
 }
 
+/**
+ * The function SaveToFile - get student name and its status.
+ * According to the student status its save his grade with his name and comment.
+ * @param name - the student name
+ * @param status - his work status
+ * @param firstStudent - boolean variable that holds if its the first student
+ */
 void SaveToFile(const char* name, int status, bool firstStudent){
+    // Initial the line that will be written to the file
     char line[MAX_CHAR_IN_LINE]={};
+    // If its not the first student, add enter to separate between the students.
     if(!firstStudent){
-        strcat(line,ENTER);
+       strcat(line,ENTER);
     }
-    strcpy(line,name);
+    strcat(line,name);
     strcat(line,COMMA);
+    // Open and check the result file
     int fd = open(RESULT_FILE,O_WRONLY|O_APPEND|O_CREAT,PERMISSION);
     if (fd == SYSTEM_CALL_FAILURE) {
         EndProgram();
     }
+    /*
+     * According to the student status,
+     * It set the appropriate grade and comment.
+    */
     switch (status){
         case (NO_C_FILE):
             strcat(line,NO_C_GRADE);
@@ -309,71 +363,113 @@ void SaveToFile(const char* name, int status, bool firstStudent){
         default:
             break;
     }
-    write(fd,line,strlen(line));
+    // write to the result file the line and check
+    if(write(fd,line,strlen(line))!=strlen(line)){
+        EndProgram();
+    }
 }
 
 
+/**
+ * main program - get configuration file,
+ * Take from it path to students directory, input file text,
+ * And correct output file txt.
+ * The program search the C file in every student directory,
+ * And give every student grade for its work.
+ * The grades save in result.csv file.
+ */
 int main(int argc, char* argv[]) {
+    // Counter number of students
+    int counter = INITIAL_VALUE;
+    // flag for the first student
+    bool firstStudent = true;
+    // Checks the valid of the input
     if(argc!= NUMBER_OF_ELEMENTS){
         printf(INPUT_ERROR);
         return ERROR;
     }
+    // Get the path from the command to the configuration file
     char* configPath = argv[FILE_PLACE];
+    // Initial the variables that will hold the three paths
     char directory[MAX_CHAR_IN_LINE] = {};
     char input[MAX_CHAR_IN_LINE] = {};
     char output[MAX_CHAR_IN_LINE] = {};
-
+    // Initial the variable with the paths
     getPaths(configPath,directory,input,output);
-
     struct dirent* pDirent;
     DIR* pDir = opendir(directory);
+    // Checks the system call opendir
     if(!pDir){
         EndProgram();
     }
-
+    // Scan all the sub directorys inside the given directory
     while ((pDirent=readdir(pDir))!= NULL){
+        // Checks that its not the current or the previous directory (. / ..)
         if(strcmp(pDirent->d_name,CURRENT_DIR)!=SUBTRACTION &&
            strcmp(pDirent->d_name,PREVIOUS_DIR)!=SUBTRACTION){
+            counter++;
+            // Checks if its the first student
+            if(counter>SUCCESS){
+                firstStudent = false;
+            }
+            // Set the current path
             char directoryPath[MAX_CHAR_IN_LINE]={};
             strcpy(directoryPath,directory);
             char studentName[MAX_CHAR_IN_LINE]={};
             strcpy(studentName,pDirent->d_name);
-
             char studentCFilePath[MAX_CHAR_IN_LINE]={};
             strcat(directoryPath,NEXT_PATH);
             strcat(directoryPath,pDirent->d_name);
-
+            // Search C file inside the directory path (student directory)
             searchFileInDirectory(directoryPath,studentCFilePath);
-
             // In case that c file exist
             if(strlen(studentCFilePath)!=EMPTY_PATH){
+                // Compile the file
                 int compile = CompileStudentFile(studentCFilePath);
+                // Checks if the compilation succeeded
                 if(compile){
-                    int a = RunStudentFile(input,studentName);
+                    // Run the compiled program
+                    int programEnd = RunStudentFile(input,studentName);
                     // creating the path for the program output file
                     char programOutputPath[MAX_CHAR_IN_LINE] = {};
-                    strcpy(programOutputPath,getcwd(programOutputPath,sizeof(programOutputPath)));
+                    // Get the current directory and check
+                    if(getcwd(programOutputPath,sizeof(programOutputPath))==NULL){
+                        EndProgram();
+                    }
                     strcat(programOutputPath,NEXT_PATH);
                     strcat(programOutputPath,studentName);
                     strcat(programOutputPath,END_OF_TXT);
-                    // in case that the program running didn't took 5 sec
-                    if(a){
+                    // in case that the program running time took maximum 5 sec
+                    if(programEnd){
+                        // Compare the correct output file with the program output file
                         int compare = CompareOutput(output,programOutputPath);
-                        SaveToFile(studentName,compare);
+                        // Save the student grade in the result file
+                        SaveToFile(studentName,compare,firstStudent);
                     } else {
-                        SaveToFile(studentName,TIMEOUT);
+                        // Save the student grade in the result file
+                        SaveToFile(studentName,TIMEOUT,firstStudent);
                     }
+                    // Remove the program output file
                     unlink(programOutputPath);
                 }
                 else{
-                    SaveToFile(studentName,COMPILATION_ERROR);
+                    // Save the student grade in the result file
+                    SaveToFile(studentName,COMPILATION_ERROR,firstStudent);
                 }
             } else{
-                SaveToFile(studentName,NO_C_FILE);
+                // Save the student grade in the result file
+                SaveToFile(studentName,NO_C_FILE,firstStudent);
             }
         }
     }
-    unlink(COMPILED_FILE_NAME);
-    closedir(pDir);
+    // Remove the compiled program and check
+    if(unlink(COMPILED_FILE_NAME)==SYSTEM_CALL_FAILURE){
+        EndProgram();
+    }
+    // Close the directory and check
+    if(closedir(pDir)==SYSTEM_CALL_FAILURE){
+        EndProgram();
+    }
+    // The program finished :)
     return INITIAL_VALUE;
 }
